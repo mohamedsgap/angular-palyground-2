4243225077c637257ca845767320003ff86710e8
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2022-01-13 00:37:51 +0200

    play with String Interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,4 +4,12 @@ import { Component } from '@angular/core';
   selector: 'app-server',
   templateUrl: './server.component.html',
 })
-export class ServerComponent {}
+export class ServerComponent {
+  serverId: number = 1234;
+  serverId2: number = 5678;
+  serverStatus: string = 'online';
+
+  againServerStatus() {
+    return 'offline';
+  }
+}
