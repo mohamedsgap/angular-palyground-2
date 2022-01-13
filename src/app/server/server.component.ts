2fe818de67374e096a65384e5a9bcf7f4b1375c0
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2022-01-13 02:47:37 +0200

    play with ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css'],
 })
 export class ServerComponent {
   serverId: number = 1234;
