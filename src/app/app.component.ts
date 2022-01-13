3ee13a17aa484a3106c22e643bfaec980a1215f4
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2022-01-13 03:52:49 +0200

    solve assignment on directives ngFor, ngIf, ngClass, ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,20 @@ export class AppComponent {
   title: string = 'angular-course-app';
   username: string = '';
   isBtnDisabled: boolean = this.username.length === 0;
-
+  showDetails: boolean = false;
+  counter: number = 0;
+  list: number[] = [];
   resetUsername() {
     this.username = '';
+  }
+
+  showTextDetails() {
+    this.showDetails = !this.showDetails;
+    this.counter = this.counter + 1;
+    this.list.push(this.counter);
+  }
+
+  changeBackgroudColor(num: number) {
+    return num >= 5 ? 'blue' : 'orange';
   }
 }
