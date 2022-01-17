d0d38c65ab35918cf6df1e1658be558d461ab3d8
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2022-01-18 00:08:39 +0200

    sync updates after switching brnaches

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-course-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-course-app');
+    expect(app.title).toEqual('Angular Course Project');
   });
 
   it('should render title', () => {
