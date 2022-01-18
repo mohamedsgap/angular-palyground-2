beb0f0a7e8f58f8e5166817042e699231a15ef6a
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2022-01-19 00:19:59 +0200

    play with component life cycle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component, ViewChild } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [];
+  title: string = 'first value ever';
   @ViewChild('inputRefWithViewChild') inputRefWithViewChild;
 
   onServerCreated(serverData) {
@@ -20,7 +21,59 @@ export class AppComponent {
   logRefVal(inputRef) {
     console.log('log ref', inputRef, inputRef.value);
   }
+
   logRefVal2() {
     console.log('log ref with viewChild', this.inputRefWithViewChild);
+  }
+
+  ngOnChanges(changes) {
+    this.title = 'value changed in ngOnChanges';
+    console.log('ngOnChanges called!');
+    console.log(changes);
+    console.log('onChange', this.title);
+  }
+
+  ngOnInit() {
+    this.title = 'value changed in ngOnInit';
+    console.log('ngOnInit called!');
+    console.log('onInit', this.title);
+  }
+
+  ngDoCheck() {
+    this.title = 'value changed in ngDoCheck';
+    console.log('ngDoCheck called!');
+    console.log('onDoCheck', this.title);
+  }
+
+  ngAfterContentInit() {
+    this.title = 'value changed in ngAfterContentInit';
+
+    console.log('ngAfterContentInit called!');
+    console.log('AfterContentInit', this.title);
+  }
+
+  ngAfterContentChecked() {
+    this.title = 'value changed in ngAfterContentChecked';
+
+    console.log('ngAfterContentChecked called!');
+    console.log('AfterContentChecked', this.title);
+  }
+
+  ngAfterViewInit() {
+    this.title = 'value changed in ngAfterViewInit';
+
+    console.log('ngAfterViewInit called!');
+    console.log('AfterViewChecked', this.title);
+  }
+
+  ngAfterViewChecked() {
+    this.title = 'value changed in ngAfterViewChecked';
+
+    console.log('ngAfterViewChecked called!');
+    console.log('AfterViewChecked', this.title);
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy called!');
   }
 }
